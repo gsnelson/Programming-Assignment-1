54c33603838597b4a08307b8e16cc0ee0cd0a1e4
gsnelson <gscott_nelson@hotmail.com>
2020-03-16 13:11:45 -0400

    Initial commit. Vouched function produces the same results as the examples provided.

---
+++ pollutantmean.R
@@ -0,0 +1,28 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  path <- file.path(getwd(), directory, fsep = .Platform$file.sep)
+  totals <- c()
+
+  for (i in id) {
+    if (i < 10) {
+      data <-
+        read.csv(paste(path, .Platform$file.sep, '00', i, '.csv', sep = ''))
+    }
+
+    else if (i < 100) {
+      data <-
+        read.csv(paste(path, .Platform$file.sep, '0', i, '.csv', sep = ''))
+    }
+
+    else {
+      data <-
+        read.csv(paste(path, .Platform$file.sep, i, '.csv', sep = ''))
+    }
+
+    bad <- is.na(data[pollutant])
+    scores <- data[pollutant][!bad]
+    totals <- append(totals, scores, after = length(totals))
+  }
+  mean(totals)
+}
+
+pollutantmean('specdata', 'nitrate', 23)
